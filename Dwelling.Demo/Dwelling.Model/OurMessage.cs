ff08f60bb933e3aa37a8755e75cd50f7f94394e4
dlbayue6 <45560387+dlbayue6@users.noreply.github.com>
2018-12-06 15:20:25 +0800

    Create OurMessage.cs

---
+++ Dwelling.Demo/Dwelling.Model/OurMessage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dwelling.Model
+{
+    //项目信息表（关于我们）
+   public  class OurMessage
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// 版本
+        /// </summary>
+        public int Version { get; set; }
+        /// <summary>
+        /// 项目名称
+        /// </summary>
+        public int ProjectName { get; set; }
+        /// <summary>
+        /// 官方网站
+        /// </summary>
+        public int OfficialWebsite { get; set; }
+        /// <summary>
+        /// 客服电话
+        /// </summary>
+        public int ServiceTel { get; set; }
+  
+    }
+}
