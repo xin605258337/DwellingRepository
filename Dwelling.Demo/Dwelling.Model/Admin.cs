5826ccbafb0665bdbdb7a000095d32e9edaaa5de
xinxin <605258337@qq.com>
2018-12-06 09:44:48 +0800

    12.06.02
    1

---
+++ Dwelling.Demo/Dwelling.Model/Admin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dwelling.Model
+{
+    //管理员表
+    public class Admin
+    {
+        /// <summary>
+        ///管理员ID 
+        /// </summary>
+        public int Admin_ID { get; set; }
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Admin_Name { get; set; }
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Admin_Password { get; set; }
+        /// <summary>
+        /// 身份
+        /// </summary>
+        public int Admin_Permission { get; set; }
+    }
+}
---
+++ Dwelling.Demo/Dwelling.Services/AdminService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dwelling.Services
+{
+    using System.Configuration;
+    //理员Service
+    public class AdminService
+    {
+        //连接数据库字符串
+        private static readonly string connStr= ConfigurationManager.ConnectionStrings["DwellingDB"].ConnectionString;
+    }
+}
