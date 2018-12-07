f12cd0117bf7ec78056373e63fc0a8ecb6350064
xinxin <605258337@qq.com>
2018-12-07 13:25:18 +0800

    12.07.02

--- Dwelling.Demo/Dwelling.UI/Controllers/HouseController.cs
+++ Dwelling.Demo/Dwelling.UI/Controllers/HouseController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,6 +11,7 @@ namespace Dwelling.UI.Controllers
 {
     public class HouseController : Controller
     {
+
         // GET: House
         public ActionResult Index()
         {
@@ -21,34 +25,9 @@ namespace Dwelling.UI.Controllers
         {
             return View();
         }
-        /// <summary>
-        /// 多图片上传
-        /// </summary>
-        /// <param name="context"></param>
-        [HttpPost]
-        public void ProcessRequest(HttpContext context)
-        {
-            context.Response.ContentType = "text/plain";
 
-            var num = context.Request.Files.Count;
+        
 
-            for (int i = 0; i < num; i++)
-            {
-                HttpPostedFile file = context.Request.Files[i];
-                //上传的文件保存到目录(为了保证文件名不重复，加个Guid)
-                string path = "~/Content/Img/" + Guid.NewGuid().ToString() + file.FileName;
-                file.SaveAs(context.Request.MapPath(path));//必须得是相对路径
-
-            }
-            context.Response.Write("上传成功");
-        }
-
-        public bool IsReusable
-        {
-            get
-            {
-                return false;
-            }
-        }
+       
     }
 }
