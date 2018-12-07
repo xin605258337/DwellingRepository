4f92c9bed5bd121b1b16bb7256cd41d3ea8156c5
YoonaZBJ <45481297+YoonaZBJ@users.noreply.github.com>
2018-12-07 08:25:36 +0800

    ZBJ12.0702

---
+++ Dwelling.Demo/Dwelling.IServices/IHabitableroomServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dwelling.IServices
+{
+    using Dwelling.Model;
+    //户型接口
+    public interface IHabitableroomServices
+    {
+         List<HabitableRoom> GetHabitableRoom();
+    }
+}
---
+++ Dwelling.Demo/Dwelling.Services/HabitableRoomServices.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dwelling.Services
+{
+    using System.Configuration;
+    using Dwelling.Model;
+    using MySql.Data;
+    using MySql.Data.MySqlClient;
+    using System.Data;
+    using Dapper;
+    using Dwelling.IServices;
+
+    public class HabitableRoomServices : IHabitableroomServices
+    {
+        private static readonly string connStr = ConfigurationManager.ConnectionStrings["DwellingDB"].ConnectionString;
+        MySqlConnection conn = null;
+        public HabitableRoomServices()
+        {
+            if (conn == null)
+            {
+                conn = new MySqlConnection(connStr);
+            }
+        }
+        /// <summary>
+        /// 获取户型信息
+        /// </summary>
+        /// <returns></returns>
+        public List<HabitableRoom> GetHabitableRoom()
+        {
+            List<HabitableRoom> habitableRooms = conn.Query<HabitableRoom>("proc_gethabitableroom", null, commandType: CommandType.StoredProcedure).ToList();
+            return habitableRooms;
+        }
+
+    }
+}
