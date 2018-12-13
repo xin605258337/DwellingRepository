198d2a94a3a6aabd36eadec4eaf1d9ae0a29d019
17937458081 <45580217+17937458081@users.noreply.github.com>
2018-12-13 21:05:20 +0800

    1213王帅磊

---
+++ Dwelling.Demo/Dwelling.IServices/IPermissionService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dwelling.IServices
+{
+    using Dwelling.Model;
+    public interface IPermissionService
+    {
+        /// <summary>
+        /// 添加权限
+        /// </summary>
+        /// <param name="permission"></param>
+        /// <returns></returns>
+      int  addPermission(Permission permission);
+        /// <summary>
+        /// 获取权限
+        /// </summary>
+        /// <returns></returns>
+       List<Permission> GetPermissions();
+
+    }
+}
---
+++ Dwelling.Demo/Dwelling.Services/RoleService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dwelling.Services
+{
+    using System.Configuration;
+    using Dwelling.Model;
+    using MySql.Data;
+    using MySql.Data.MySqlClient;
+    using System.Data;
+    using Dapper;
+    using Dwelling.IServices;
+    public class RoleService:IRoleService
+    {
+        private static readonly string connStr = ConfigurationManager.ConnectionStrings["DwellingDB"].ConnectionString;
+        MySqlConnection conn = null;
+        public RoleService()
+        {
+            if (conn == null)
+            {
+                conn = new MySqlConnection(connStr);
+            }
+        }
+        /// <summary>
+        /// 添加角色
+        /// </summary>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        public int addRole(Role role)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+                string sql = string.Format("insert  into Permission values(@role_ID,@role_Name,@permission_Name)");
+                int addRole = conn.Execute(sql, role);
+                return addRole;
+
+            }
+        }
+        /// <summary>
+        /// 显示角色
+        /// </summary>
+        /// <returns></returns>
+        public List<Role> GetRole()
+        {
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+                string sql = string.Format("select * from Role");
+                IEnumerable<Role> result = conn.Query<Role>(sql, null);
+                return result.ToList<Role>();
+            }
+        }
+
+
+    }
+}
