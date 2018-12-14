50ef9085020301c59423d732a4f42b29e4e98065
17937458081 <45580217+17937458081@users.noreply.github.com>
2018-12-14 08:34:30 +0800

    1214

---
+++ Dwelling.Demo/Dwelling.Model/RolePermission.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dwelling.Model
+{
+   public class RolePermission
+    {
+        /// <summary>
+        /// 权限角色关联表
+        /// </summary>
+        public int Rolepermission_ID { get; set; }
+        /// <summary>
+        /// 权限id关联
+        /// </summary>
+        public int permission_ID { get; set; }
+        /// <summary>
+        /// 角色id关联
+        /// </summary>
+        public int role_ID { get; set; }
+    }
+}
