f27112ade3fe7eef383351c86eaf6ede1ac2011d
xinxin <605258337@qq.com>
2018-12-18 20:36:12 +0800

    12.18.02

---
+++ Dwelling.Demo/Dwelling.Common/AuthorizationAttribute.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Dwelling.Common
+{
+    using System.Web;
+    using System.Web.Security;
+    using Newtonsoft.Json;
+    using Model;
+    using System.Web.Http.Controllers;
+    using System.Web.Mvc;
+
+    public class AuthorizationAttribute: AuthorizeAttribute
+    {
+         /// <summary>
+         /// 授权过滤方法
+         /// </summary>
+         /// <param name="filterContext">授权上下文</param>
+         public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+                //已认证用户
+                var strUserData = ((FormsIdentity)filterContext.HttpContext.User.Identity).Ticket.UserData;
+                var _loginInfo = JsonConvert.DeserializeObject<Admin>(strUserData);
+                _loginInfo = RedisHelper.Get<Admin>(_loginInfo.Admin_ID.ToString());
+
+                var action = filterContext.RouteData.Values["Action"];
+                var controller = filterContext.RouteData.Values["Controller"];
+                var tmpVal = (controller + "/" + action).ToLower();
+
+                //filterContext.HttpContext.Response.Redirect("/student/index");
+            }
+            else
+            {
+                FormsAuthentication.SignOut();
+            }
+
+            // if (filterContext.HttpContext.Session["UserInfo"] == null)
+            // {
+            //      filterContext.HttpContext.Response.Redirect("/home/login");
+            //  }
+        }
+    }
+}
--- Dwelling.Demo/Dwelling.UI/Controllers/HouseController.cs
+++ Dwelling.Demo/Dwelling.UI/Controllers/HouseController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -9,13 +11,13 @@ using System.Web.Mvc;
 
 namespace Dwelling.UI.Controllers
 {
-    public class HouseController : Controller
+    public class HouseController : BaseController
     {
 
         // GET: House
         public ActionResult Index()
         {
-            return View();
+            return Content("<script>location.href='http://localhost:55909/index.html'</script>");
         }
         [HttpPost]
         public JsonResult GetImg()
@@ -25,15 +27,64 @@ namespace Dwelling.UI.Controllers
             for (int i = 0; i < num; i++)
             {
                 HttpPostedFile file = System.Web.HttpContext.Current.Request.Files[i];
-                //上传的文件保存到目录(为了保证文件名不重复，加个Guid)
-                //string path = "/Content/Img/" + Guid.NewGuid().ToString() + file.FileName;
-                //file.SaveAs(HttpContext.Request.MapPath("~"+path));//必须得是相对路径
-                //pathList.Add(".." + path);
                 string path= Guid.NewGuid().ToString() + file.FileName;
+                //压缩图片
+                getThumImage(file.FileName, 180, 30, path);
                 file.SaveAs(HttpContext.Request.MapPath("~/Content/Img/"+path));//必须得是相对路径径
                 pathList.Add(path);
+
             }
             return Json(pathList);
-        }  
+        }
+        /// <summary>  
+        /// 生成缩略图  
+        /// </summary>  
+        /// <param name="sourceFile">原始图片文件</param>  
+        /// <param name="quality">质量压缩比</param>  
+        /// <param name="multiple">收缩倍数</param>  
+        /// <param name="outputFile">输出文件名</param>  
+        /// <returns>成功返回true,失败则返回false</returns>  
+        public static bool getThumImage(String sourceFile, long quality, int multiple, String outputFile)
+        {
+            try
+            {
+                long imageQuality = quality;
+                Bitmap sourceImage = new Bitmap(sourceFile);
+                ImageCodecInfo myImageCodecInfo = GetEncoderInfo("image/jpeg");
+                System.Drawing.Imaging.Encoder myEncoder = System.Drawing.Imaging.Encoder.Quality;
+                EncoderParameters myEncoderParameters = new EncoderParameters(1);
+                EncoderParameter myEncoderParameter = new EncoderParameter(myEncoder, imageQuality);
+                myEncoderParameters.Param[0] = myEncoderParameter;
+                float xWidth = sourceImage.Width;
+                float yWidth = sourceImage.Height;
+                Bitmap newImage = new Bitmap((int)(xWidth / multiple), (int)(yWidth / multiple));
+                Graphics g = Graphics.FromImage(newImage);
+
+                g.DrawImage(sourceImage, 0, 0, xWidth / multiple, yWidth / multiple);
+                g.Dispose();
+                newImage.Save(outputFile, myImageCodecInfo, myEncoderParameters);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        /**/
+        /// <summary>  
+        /// 获取图片编码信息  
+        /// </summary>  
+        private static ImageCodecInfo GetEncoderInfo(String mimeType)
+        {
+            int j;
+            ImageCodecInfo[] encoders;
+            encoders = ImageCodecInfo.GetImageEncoders();
+            for (j = 0; j < encoders.Length; ++j)
+            {
+                if (encoders[j].MimeType == mimeType)
+                    return encoders[j];
+            }
+            return null;
+        }
     }
 }
